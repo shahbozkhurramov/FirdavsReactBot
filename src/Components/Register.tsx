9409c5f6804e2ebcae19e84a096b90cae9c0215b
shahbozkhurramov <shahbozkhurramov@gmail.com>
2023-01-05 18:40:56 +0500

    reg

---
+++ src/Components/Register.tsx
@@ -0,0 +1,40 @@
+import React, {useState, useRef} from 'react'
+
+export default function Register() {
+  return (
+    <div className="container">
+	<div className="screen">
+		<div className="screen__content">
+			<form className="login">
+				<div className="login__field">
+					<i className="login__icon fas fa-user"></i>
+					<input type="text" className="login__input" placeholder="Ismingiz"/>
+				</div>
+				<div className="login__field">
+					<i className="login__icon fas fa-lock"></i>
+					<input type="number" className="login__input" placeholder="Telefon raqamingiz"/>
+				</div>
+				<button className="button login__submit">
+					<span className="button__text">Ro'yxatdan o'tish</span>
+					<i className="button__icon fas fa-chevron-right"></i>
+				</button>				
+			</form>
+			{/* <div className="social-login">
+				<h3>log in via</h3>
+				<div className="social-icons">
+					<a href="#" className="social-login__icon fab fa-instagram"></a>
+					<a href="#" className="social-login__icon fab fa-facebook"></a>
+					<a href="#" className="social-login__icon fab fa-twitter"></a>
+				</div>
+			</div> */}
+		</div>
+		<div className="screen__background">
+			<span className="screen__background__shape screen__background__shape4"></span>
+			<span className="screen__background__shape screen__background__shape3"></span>		
+			<span className="screen__background__shape screen__background__shape2"></span>
+			<span className="screen__background__shape screen__background__shape1"></span>
+		</div>		
+	</div>
+</div>
+  )
+}
---
+++ src/Pages/Home.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Register from '../Components/Register'
+
+export default function Home() {
+  return (
+    <Register/>
+  )
+}
