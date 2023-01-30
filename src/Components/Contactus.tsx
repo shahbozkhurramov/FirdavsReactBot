f027f1dda944ee9c75f834abb51f0790d33681f7
shahbozkhurramov <shahbozkhurramov@gmail.com>
2023-01-30 16:10:31 +0500

    ok

--- src/Components/Contactus.tsx
+++ src/Components/Contactus.tsx
@@ -57,37 +57,37 @@ export default function Contactus() {
     <>
     <div className="contact-form-container" data-aos="zoom-in-up" data-aos-duration="800">
       <form className="contact-form" id='contact-form'>
-        <div className='our-location'>
+        <div className='our-location' data-aos="fade-up" data-aos-duration="1500">
             <div className="contact-card-title">Bizning manzil</div>
             <div className="contact-card-row">
-                <div className="contact-card-item"><i className="fas fa-map-marker-alt"></i> Manzil: <a href="https://yandex.com/maps/-/CCU55AxJpC">123 Main Street</a></div>
+                <div className="contact-card-item"><i className="fas fa-map-marker-alt"></i> Manzil: <a href="https://yandex.com/maps/-/CCU55AxJpC">Chorsu metrodan 10 minut</a></div>
                 <div className="contact-card-item"><i className="fas fa-phone"></i> Telefon raqam: <a href="tel:+998909228267">+998 91 788-69-95 </a></div>
                 <div className="social-icons">
-                    <a href="https://t.me/firdavs_talim" style={{color:'blue'}} target="_blank" rel='noreferrer'>
+                    <a href="https://t.me/firdavs_talim" style={{color:'#2c83e0'}} target="_blank" rel='noreferrer'>
                         <i className="fab fa-telegram fa"></i>
                     </a>
                     <a href="https://www.instagram.com/firdavs_talim" style={{color:'purple'}} target="_blank" rel='noreferrer'>
                         <i className="fab fa-instagram fa"></i>
                     </a>
-                    <a href="https://www.youtube.com/" style={{color:'red'}} target="_blank" rel='noreferrer'>
+                    <a href="https://www.youtube.com/@firdavs_talim" style={{color:'red'}} target="_blank" rel='noreferrer'>
                         <i className="fab fa-fw fa-youtube"></i>
                     </a>
-                    <a href="https://m.facebook.com/Firdavs-talim-103191505735452" target="_blank" rel='noreferrer'>
+                    <a href="https://m.facebook.com" target="_blank" rel='noreferrer'>
                         <i className="fab fa-facebook-square fa"></i>
                     </a>
                 </div>
             </div>
         </div>
-        <h2 className="form-title" data-aos="fade-down" data-aos-duration="1500">Biz bilan bog'laning</h2>
-        <div className="form-field" data-aos="fade-up" data-aos-duration="1500">
+        <h2 className="form-title" data-aos="fade-down" data-aos-duration="1000">Biz bilan bog'laning</h2>
+        <div className="form-field">
             <input type="text" id="name" ref={nameRef} placeholder="Ismingiz" name="name" onFocus={() => ScrollToInput("name")}/>
             <div className="error-message" id="nameError"></div>
         </div>
-        <div className="form-field" data-aos="fade-up" data-aos-duration="2000">
+        <div className="form-field">
             <input type="tel" id="phone" defaultValue={phoneCode} placeholder="Telefon raqamingiz" ref={phoneRef} name="phone" onFocus={() => ScrollToInput("phone")}/>
             <div className="error-message" id="phoneError"></div>
         </div>
-        <div className="form-field message-field" data-aos="fade-up" data-aos-duration="2000">
+        <div className="form-field message-field">
             <textarea id="message" ref={messageRef} placeholder="Xabar..." name="message" onFocus={() => ScrollToInput("name")}></textarea>
             <div className="error-message" id="messageError"></div>
         </div>
